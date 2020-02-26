6b374e71b75f55bb93464b2c44a2ca17e78b5a12
dadoulan <59800397+dadoulan@users.noreply.github.com>
2020-02-26 11:51:47 +0100

    Add files via upload

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,12 +6,17 @@
 /*   By: shkhan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 09:01:39 by shkhan            #+#    #+#             */
-/*   Updated: 2020/02/25 13:38:08 by shkhan           ###   ########.fr       */
+/*   Updated: 2020/02/26 11:50:37 by shkhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
 int		ft_strlen(char *str)
 {
+	int i;
+	i = 0;
 	while (str[i])
 		i++;
 	return (i);
@@ -30,6 +35,7 @@ char	*ft_strdup(char *src)
 		dup[i] = src[i];
 		i++;
 	}
+	dup[i] = '\0';
 	return (dup);
 }
 
@@ -39,17 +45,38 @@ struct s_stock_str *ft_strs_to_tab(int ac, char **av)
 	t_stock_str *t_av;
 
 	i = 0;
-	if(!(t_av = malloc(sizeof(t_av) * (ac + 1))))
+	if(!(t_av = (t_stock_str*)malloc(sizeof(*t_av) * (ac + 1))))
 		return (NULL);
-	while (i < ac)
+	while (i < ac && av[i] != 0)
 	{
-		t_av[i]->size = ft_strlen(av[i]);
-		t_av[i]->str = av[i];
-		t_av[i]->copy = ft_strdup(av[i]);
+		t_av[i].size = ft_strlen(av[i]);
+		t_av[i].str = av[i];
+		t_av[i].copy = ft_strdup(av[i]);
 		i++;
 	}
-	t_av[i]->size = 0;
-	t_av[i]->str = 0;
-	t_av[i]->copy = 0;
+	t_av[i].size = 0;
+	t_av[i].str = 0;
+	t_av[i].copy = 0;
 	return (t_av);
 }
+
+#include <stdio.h>
+int main()
+{
+	int i =0;
+	char *av[] =
+	{
+		"manger",
+		"13927",
+		"salut"
+	};
+	t_stock_str *tab;
+	tab = ft_strs_to_tab(3, av);
+	while (tab[i].size)
+	{
+		printf("%d\n %s\n %s\n", tab[i].size, tab[i].str, tab[i].copy);
+		i++;
+	}
+	return(0);
+}
+
